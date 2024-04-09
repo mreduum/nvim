43791519cbfbc120fcd13077ce9bbda9c716b732
Eduardo Mendieta <eduardomendietatenesaca@gmail.com>
2024-04-09 16:28:53 -0500

    Adding the initial basic configuration.

--- lua/mreduum/set.lua
+++ lua/mreduum/set.lua
@@ -1,1 +1,15 @@
+vim.opt.mouse = 'a'
+
 vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.termguicolors = true
+
+vim.opt.wrap = false
+
+vim.cmd('syntax enable')
